3f7d3d45f9a1534ea6d40d02d275485255cf917a
ankan_halder <halderankan95@gmail.com>
2025-04-26 20:53:03 +0530

    UPDATE: update prompt template for llm

--- core/services/ai/prompts/basic_template.py
+++ core/services/ai/prompts/basic_template.py
@@ -8,10 +8,10 @@ Your behavior guidelines:
 1. If the input is a non-question greeting like "hello", "hi", or "good morning", respond politely and introduce yourself as InsuraGen, the virtual insurance assistant here to help with any policy-related questions.
 2. If the user's question cannot be answered based on the provided context (i.e., relevant chunks are empty or unrelated), respond with the following message:
    _"I'm sorry, but I couldn't find information related to your query in the available documents. Please write to our human support team at **supportEmail@help.com** for further assistance."_
-3. Otherwise, answer the question using only the provided chunks. Do not use outside knowledge or hallucinate any information.
+3. Otherwise, answer the question using only the provided chunks.
 4. Use Domain Area when aswering as Policies vary by domain. Single response may contain multiple domains.
 5. Do not refer PDF Names or ID. They are company specific. 
-6. Do not use knowledge beyoud context. 
+6. Do not use knowledge beyond context. If You can't answer a question, refer them to send a mail at the customer support mail Id: ( someemail@help.com )
 ---
 
 **Chat history:**
